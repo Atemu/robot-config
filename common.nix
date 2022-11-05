bc7388ead278820cae7a193993ec63ac6c989c8e
Atemu <atemu.main@gmail.com>
2022-11-05 21:25:41 +0100

    Common: Disable clang-tidy

--- common.nix
+++ common.nix
@@ -11,6 +11,9 @@
     ./0001-always-have-screenOrientation-SCREEN_ORIENTATION_UNS.patch
   ];
 
+  # I don't need clang-tidy checks, I'm just building what others have made and verified
+  source.dirs."build/soong".postPatch = ''substituteInPlace cc/tidy.go --replace "flags.Tidy = true" "flags.Tidy = false"'';
+
   ccache.enable = true;
   ccache.directory = "/Volumes/Data/Ccache";
 }

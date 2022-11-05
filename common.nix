d3800b3d09b4caddc8db491e81152f4355f3bb18
Atemu <atemu.main@gmail.com>
2022-11-05 19:08:46 +0100

    Common: Enable ccache

--- common.nix
+++ common.nix
@@ -10,4 +10,7 @@
   source.dirs."frameworks/base".patches = lib.mkIf (config.androidVersion == 12) [
     ./0001-always-have-screenOrientation-SCREEN_ORIENTATION_UNS.patch
   ];
+
+  ccache.enable = true;
+  ccache.directory = "/Volumes/Data/Ccache";
 }

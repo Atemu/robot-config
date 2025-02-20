aaa23d30b28ece37329362db068589d7fcdb9693
Atemu <git@atemu.net>
2025-02-20 17:58:06 +0100

    common: refactor

--- common.nix
+++ common.nix
@@ -7,14 +7,18 @@
 
   microg.enable = true;
 
-  source.dirs."frameworks/base".patches = lib.optionals (config.androidVersion == 12) [
-    ./screenOrientation-19.patch
-  ] ++ lib.optionals (config.androidVersion == 13)  [
-    ./screenOrientation-20.patch
+  source.dirs."frameworks/base".patches = lib.mkMerge [
+    (lib.mkIf (config.androidVersion == 12) [
+      ./screenOrientation-19.patch
+    ])
+    (lib.mkIf (config.androidVersion == 13) [
+      ./screenOrientation-20.patch
+    ])
   ];
 
   # I don't need clang-tidy checks, I'm just building what others have made and verified
-  source.dirs."build/soong".postPatch = ''substituteInPlace cc/tidy.go --replace "flags.Tidy = true" "flags.Tidy = false"'';
+  source.dirs."build/soong".postPatch =
+    ''substituteInPlace cc/tidy.go --replace "flags.Tidy = true" "flags.Tidy = false"'';
 
   ccache.enable = true;
 }
